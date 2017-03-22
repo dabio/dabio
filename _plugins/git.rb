0f1d935c625e2616ef24b2066b732e6eaf62df61
Danilo <dabio@users.noreply.github.com>
2017-03-22 09:46:22 +0100

    Fixing Hound warnings

--- _plugins/git.rb
+++ _plugins/git.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
 module Jekyll
+  # GitGenerator will give you basic information about the current git
+  # status. Currently only the number of `commits` and the current commit
+  # `hash` is implemented.
   class GitGenerator < Generator
     priority :high
     safe true
 
     def generate(site)
-      site.data['hash'] = %x( git rev-parse --short HEAD ).strip
-      site.data['commits'] = %x( git rev-list HEAD --count ).strip
+      site.data['hash'] = `git rev-parse --short HEAD`.strip
+      site.data['commits'] = `git rev-list HEAD --count`.strip
     end
   end
 end
